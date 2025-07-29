acaf81df0a4f6816804c124d50a11ee13cd7f542
ParryOgbemudia <parryogbemudia@gmail.com>
2025-07-29 15:51:07 +0100

    removed vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,13 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import svgr from "vite-plugin-svgr";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
