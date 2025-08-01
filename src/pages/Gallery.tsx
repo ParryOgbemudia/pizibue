1302e07632c06b5305928e27a76020b215b6abba
ParryOgbemudia <parryogbemudia@gmail.com>
2025-08-01 15:29:01 +0100

    ellipse for gallery slider done with Array.from

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -2,11 +2,14 @@ import HeadingMobile from "../components/HeadingMobile";
 import gallery_img1 from "@/assets/gallery/gallery_img1.webp";
 import Button from "../components/ui/Button";
 
+const totalSlides = 8;
+const activeIndex = 2;
+
 export default function Gallery() {
   return (
     <section className="w-full">
       <div className="mx-auto max-w-[1440px] px-4 py-14">
-        <div className="flex flex-col justify-center gap-10">
+        <div className="flex flex-col items-center justify-center gap-10">
           <HeadingMobile
             h3="Inside the Farm: Real Moments"
             span="Real Work"
@@ -19,12 +22,14 @@ export default function Gallery() {
               className="rounded-2xl"
             />
             <div className="flex items-center justify-center gap-3">
-              <button className="bg-primary h-[16px] w-[16px] rounded-full"></button>
-              <button className="h-[16px] w-[16px] rounded-full bg-[#E2E2E2]"></button>
-              <button className="h-[16px] w-[16px] rounded-full bg-[#E2E2E2]"></button>
-              <button className="h-[16px] w-[16px] rounded-full bg-[#E2E2E2]"></button>
-              <button className="h-[16px] w-[16px] rounded-full bg-[#E2E2E2]"></button>
-              <button className="h-[16px] w-[16px] rounded-full bg-[#E2E2E2]"></button>
+              {Array.from({ length: totalSlides }).map((_, i) => (
+                <button
+                  key={i}
+                  className={`h-3 w-3 rounded-full transition-colors duration-300 ${
+                    i === activeIndex ? "bg-primary" : "bg-[#E2E2E2]"
+                  }`}
+                />
+              ))}
             </div>
           </div>
 
