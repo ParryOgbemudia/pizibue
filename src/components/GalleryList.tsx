10de35249a4fa4fc70dbc5cd704ef96d9575abef
ParryOgbemudia <parryogbemudia@gmail.com>
2025-08-04 14:02:13 +0100

    footer done

--- src/components/GalleryList.tsx
+++ src/components/GalleryList.tsx
@@ -1,26 +1,26 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-import gallery_img1 from "@/assets/gallery/gallery_img1.webp";
-import gallery_img2 from "@/assets/gallery/gallery_img2.webp";
-import gallery_img3 from "@/assets/gallery/gallery_img3.webp";
-import gallery_img4 from "@/assets/gallery/gallery_img4.webp";
-import gallery_img5 from "@/assets/gallery/gallery_img5.webp";
-import gallery_img6 from "@/assets/gallery/gallery_img6.webp";
-import gallery_img7 from "@/assets/gallery/gallery_img7.webp";
+// import gallery_img1 from "@/assets/gallery/gallery_img1.webp";
+// import gallery_img2 from "@/assets/gallery/gallery_img2.webp";
+// import gallery_img3 from "@/assets/gallery/gallery_img3.webp";
+// import gallery_img4 from "@/assets/gallery/gallery_img4.webp";
+// import gallery_img5 from "@/assets/gallery/gallery_img5.webp";
+// import gallery_img6 from "@/assets/gallery/gallery_img6.webp";
+// import gallery_img7 from "@/assets/gallery/gallery_img7.webp";
 
-const images = [
-  gallery_img1,
-  gallery_img2,
-  gallery_img3,
-  gallery_img4,
-  gallery_img5,
-  gallery_img6,
-  gallery_img7,
-];
+// const images = [
+//   gallery_img1,
+//   gallery_img2,
+//   gallery_img3,
+//   gallery_img4,
+//   gallery_img5,
+//   gallery_img6,
+//   gallery_img7,
+// ];
 
-export default function GalleryList() {
-  const [currentIndex, setCurrentIndex] = useState<number | null>(0);
+// export default function GalleryList() {
+//   const [currentIndex, setCurrentIndex] = useState<number | null>(0);
 
-  useEffect(()=>{}[])
-  return <div></div>;
-}
+//   useEffect(()=>{}[])
+//   return <div></div>;
+// }
