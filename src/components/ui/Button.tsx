26ee458549c631f439e90713b48e8aeb6a963198
ParryOgbemudia <parryogbemudia@gmail.com>
2025-08-08 23:17:45 +0100

    react hook form integration

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,9 +4,15 @@ type ButtonProps = {
   children: React.ReactNode;
   onClick?: () => void;
   className?: string;
+  type?: string;
 };
 
-export default function Button({ children, className, onClick }: ButtonProps) {
+export default function Button({
+  children,
+  className,
+  onClick,
+  type = "submit",
+}: ButtonProps) {
   const base =
     "py-3.5 rounded-full text-white bg-primary leading-[1.2] flex items-center justify-center gap-2 cursor-pointer";
 
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,29 @@
+import { useForm, type SubmitHandler } from "react-hook-form";
 import Button from "../components/ui/Button";
 
+type FormFields = {
+  fullname: string;
+  email: string;
+  subject: string;
+  description: string;
+};
+
 export default function ContactUs() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormFields>();
+
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      console.log(data);
+    } catch (error) {
+      console.error("Form submission failed:", error);
+    }
+  };
+
   return (
     <section className="w-full bg-black text-white">
       <div className="mx-auto pt-34 pb-7.5 max-md:px-4 sm:px-10 md:px-16 md:pb-8 lg:px-20">
@@ -15,66 +38,108 @@ export default function ContactUs() {
             </p>
           </div>
           <form
-            action=""
+            onSubmit={handleSubmit(onSubmit)}
             className="xmd:p-10 rounded-3xl bg-[#181818] px-4 pt-8 pb-10"
           >
             <div className="flex flex-col gap-10">
               <div className="grid gap-5">
                 <div className="flex flex-col gap-2">
                   <label
-                    htmlFor=""
+                    htmlFor="fullname"
                     className="text-[14px] font-normal text-[#818981]"
                   >
                     Full Name:
                   </label>
                   <input
+                    {...register("fullname", {
+                      required: "Full name is required",
+                      validate: (value) =>
+                        value.trim().split(/\s+/).length >= 2 ||
+                        "Please enter at least two words",
+                    })}
+                    id="fullname"
                     type="text"
                     placeholder="Enter Your Full Name"
                     className="rounded-[8px] border border-[#494949] px-3 py-3.5 font-normal outline-none placeholder:text-[#818981]"
                   />
+                  {errors.fullname && (
+                    <p className="text-sm text-red-500">
+                      {errors.fullname.message}
+                    </p>
+                  )}
                 </div>
                 <div className="flex flex-col gap-2">
                   <label
-                    htmlFor=""
+                    htmlFor="email"
                     className="text-[14px] font-normal text-[#818981]"
                   >
                     Email Address:
                   </label>
                   <input
+                    {...register("email", {
+                      required: "Please enter a valid email address",
+                    })}
+                    id="email"
                     type="email"
                     placeholder="Enter Email Address"
                     className="rounded-[8px] border border-[#494949] px-3 py-3.5 font-normal outline-none placeholder:text-[#818981]"
                   />
+                  {errors.email && (
+                    <div className="text-sm text-red-500">
+                      {errors.email.message}
+                    </div>
+                  )}
                 </div>
                 <div className="flex flex-col gap-2">
                   <label
-                    htmlFor=""
+                    htmlFor="subject"
                     className="text-[14px] font-normal text-[#818981]"
                   >
                     Subject:
                   </label>
                   <input
+                    {...register("subject", {
+                      required: "Subject is required",
+                    })}
+                    id="subject"
                     type="text"
                     placeholder="Enter Subject"
                     className="rounded-[8px] border border-[#494949] px-3 py-3.5 font-normal outline-none placeholder:text-[#818981]"
                   />
+                  {errors.subject && (
+                    <div className="text-sm text-red-500">
+                      {errors.subject.message}
+                    </div>
+                  )}
                 </div>
                 <div className="flex flex-col gap-2">
                   <label
-                    htmlFor=""
+                    htmlFor="description"
                     className="text-[14px] font-normal text-[#818981]"
                   >
                     Description:
                   </label>
                   <textarea
+                    {...register("description", {
+                      required: "Description is required",
+                    })}
+                    id="description"
                     placeholder="What Do You Need?"
                     className="h-[145px] resize-none rounded-[8px] border border-[#494949] px-3 py-3.5 font-normal outline-none placeholder:text-[#818981]"
                   ></textarea>
+                  {errors.description && (
+                    <p className="text-sm text-red-500">
+                      {errors.description.message}
+                    </p>
+                  )}
                 </div>
               </div>
               <div className="xmd:self-end self-stretch">
-                <Button className="xmd:w-max flex w-full items-center justify-center px-4">
-                  Send Message
+                <Button
+                  type="submit"
+                  className="xmd:w-max flex w-full items-center justify-center px-4"
+                >
+                  {isSubmitting ? "Submitting..." : "Send Message"}
                 </Button>
               </div>
             </div>
